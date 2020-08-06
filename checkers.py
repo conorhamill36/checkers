37407c89e917524acf1f3e09807f0c6c192b42ea
conorhamill36 <conor.hamill@ed.ac.uk>
2020-08-06 13:10:11 +0100

    First commit, with hello world in checkers.py

---
+++ checkers.py
@@ -0,0 +1,7 @@
+#Program to simulate a game of checkers.
+#Initially between two simulated players making random choices (prioritising capturing opponents pieces), and then moving on to user input.
+
+def main():
+    print("Hello World")
+
+main()    
