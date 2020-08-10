e63c2171c573d239c54687f84b3d942de1e5c68a
conorhamill36 <conor.hamill@ed.ac.uk>
2020-08-10 18:21:30 +0100

    checkers_decorations now preserve prpoerties of function being called, via functools.wraps()

--- checkers_decorators.py
+++ checkers_decorators.py
@@ -1,9 +1,11 @@
 #Decorators to be used with checkers.py
+import functools
 import time
 import os, sys
 
 #Decorator that simply prints the name of the function before and after executing
 def func_name(func):
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
         print("\n>>Function being called is {}()\n".format(func.__name__))
         value = func(*args, **kwargs)
@@ -14,16 +16,18 @@ def func_name(func):
 
 #Decorator to pause at the end of a function
 def dramatic_pause(func):
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
         value = func(*args, **kwargs)
         print("Pausing for {} seconds...".format(2))
-        time.sleep(2)
+        time.sleep(0)
         return value
     return wrapper
 
 
 #Decorator to suppress printing of a function
 def mute_printing(func):
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
         sys.stdout = open(os.devnull, 'w') #Disables printing by changing stdout
         value = func(*args, **kwargs)
